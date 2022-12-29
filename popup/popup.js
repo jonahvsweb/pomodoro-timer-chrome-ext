2631d9e241262d32abdd855f50031c7fc25c7cd2
Jonah Bitautas <jonahvsweb@users.noreply.github.com>
2022-12-28 22:47:15 -0600

    Add array to capture tasks

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,4 @@
+const tasks = [];
 const addTaskBtn = document.getElementById("add-task-btn");
 
 addTaskBtn.addEventListener("click", () => addTask());
@@ -7,11 +8,21 @@ function addTask() {
   const text = document.createElement("input");
   const deleteBtn = document.createElement("input");
   const taskContainer = document.getElementById("task-container");
+  const tasksNum = tasks.length;
 
+  tasks.push("");
+
+  text.type = "text";
   text.placeholder = "Enter a task";
+  text.addEventListener("change", () => {
+    tasks[tasksNum] = text.value;
+  });
 
   deleteBtn.type = "button";
   deleteBtn.value = "X";
+  deleteBtn.addEventListener("click", () => {
+    tasks.splice(tasksNum, 1);
+  });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
