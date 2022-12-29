67d4995084f04b6b8aaecfd9dad3722ea56d462a
Jonah Bitautas <jonahvsweb@users.noreply.github.com>
2022-12-28 23:01:51 -0600

    Add task adding and deletion

--- popup/popup.js
+++ popup/popup.js
@@ -3,29 +3,49 @@ const addTaskBtn = document.getElementById("add-task-btn");
 
 addTaskBtn.addEventListener("click", () => addTask());
 
-function addTask() {
+function renderTask(taskNum) {
   const taskRow = document.createElement("div");
   const text = document.createElement("input");
   const deleteBtn = document.createElement("input");
   const taskContainer = document.getElementById("task-container");
-  const tasksNum = tasks.length;
-
-  tasks.push("");
 
   text.type = "text";
   text.placeholder = "Enter a task";
+  text.value = tasks[taskNum];
   text.addEventListener("change", () => {
-    tasks[tasksNum] = text.value;
+    tasks[taskNum] = text.value;
   });
 
   deleteBtn.type = "button";
   deleteBtn.value = "X";
   deleteBtn.addEventListener("click", () => {
-    tasks.splice(tasksNum, 1);
+    deleteTask(taskNum);
   });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
 
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const taskNum = tasks.length;
+
+  tasks.push("");
+  renderTask(taskNum);
+}
+
+function deleteTask(taskNum) {
+  tasks.splice(taskNum, 1);
+  renderTasks(taskNum);
+}
+
+function renderTasks(taskNum) {
+  const taskContainer = document.getElementById("task-container");
+
+  taskContainer.textContent = "";
+
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
 }
